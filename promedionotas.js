f3dd3129c6d0a69a8c10ced99147fb47964b521d
Fer Rodriguez <32382866+FeRodriguezAut@users.noreply.github.com>
2025-10-27 18:52:51 -0500

    Promedio de notas 1

---
+++ promedionotas.js
@@ -0,0 +1,9 @@
+// Función para calcular el promedio de un estudiante
+function calcularPromedio(notas) {
+    let suma = 0;
+    for (let i = 0; i < notas.length; i++) {
+        suma += notas[i];
+    }
+    return suma / notas.length;
+}
+
