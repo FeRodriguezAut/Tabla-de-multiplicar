ded4dd7437dc9ac577e7f07a3fc5eed825a41773
Fer Rodriguez <32382866+FeRodriguezAut@users.noreply.github.com>
2025-10-27 20:37:52 -0500

    Total compra

--- tienda.js
+++ tienda.js
@@ -12,3 +12,21 @@ function iniciarTienda() {
             alert("Precio inválido. Debe ingresar un número mayor a 0");
             precioProducto = parseFloat(prompt("Ingrese el precio del producto:"));
         }
+         let descuento = 0;
+    
+    if (totalCompra > 100000) {
+        descuento = totalCompra * 0.10;
+        alert("¡Felicidades! Has obtenido un 10% de descuento por compras superiores a $100.000");
+    }
+    
+    let totalFinal = totalCompra - descuento;
+    
+    console.log("Subtotal: $" + totalCompra);
+    console.log("Descuento aplicado: $" + descuento);
+    console.log("TOTAL A PAGAR: $" + totalFinal);
+    
+    
+    alert("Total a pagar: $" + totalFinal + "\nGracias por su compra!");
+}
+
+iniciarTienda();
